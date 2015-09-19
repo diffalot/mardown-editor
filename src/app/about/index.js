7413bb2e906cb85eadd064fd5da137473eb5d6ec
Andrew Davis <andrew@diff.mx>
2015-09-19 10:11:58 -0500

    display the readme file on the about page

--- src/app/about/index.js
+++ src/app/about/index.js
@@ -4,8 +4,11 @@ import uirouter from 'angular-ui-router'
 import routing from './about.routes'
 import AboutController from './about.controller'
 
+import hcMarked from 'angular-marked'
+
 export default angular.module('about', [
-  uirouter
+  uirouter,
+  hcMarked
 ])
   .config(routing)
   .controller('AboutController', AboutController)
